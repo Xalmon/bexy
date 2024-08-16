f692a4f696860fa078448aa5493c4288e3194ecd
Xalmon <xalmonoyefule@gmail.com>
2024-08-16 13:27:50 +0100

    create a springboot project from bash, push to git and auto create branches and push to dockerhub

---
+++ src/main/java/com.example.bexy/bexyApplication.java
@@ -0,0 +1,12 @@
+package com.example.bexy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class bexyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(bexyApplication.class, args);
+  }
+}
